190ca1447f4e46f7cdcc0a3c5d0b014f7133ce06
rakshan <rakshan@techmust.com>
2018-06-12 14:38:51 +0530

    renames . war file

--- src/main/java/com/techmust/Controller/LoginController.java
+++ src/main/java/com/techmust/Controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.techmust.Controller;
 
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController 
@@ -13,9 +18,13 @@ public class LoginController
 		return "Login";
 	}
 	
-    /*
-    @RequestMapping(value="/login/", method = RequestMethod.POST, produces = "application/json")
+    
+    @RequestMapping(value="/loginReturn/", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity login(@RequestParam(value = "username", defaultValue = "World") String name) {
         return ResponseEntity.ok(createResponse(name));
-    }*/
+    }
+    
+    private String createResponse(String name) {
+        return new JSONObject().put("Output", String.format(name)).toString();
+    }
 }
